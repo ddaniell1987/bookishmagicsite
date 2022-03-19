b4b88520a09ccfba541a3b25a0955ae5a782a726
Dominique <ddaniell1987@gmail.com>
2022-03-19 18:10:48 -0400

    updated blog

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const menu = document.querySelector('.menu');
-const navigation = document.querySelector('.navigation');
-menu.onclick = function menu(){
-    menu.classList.toggle('active');
-    navigation.classList.toggle('active');
 
-};
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+$(document).ready(function(){
+    $('.filter-item').click(function(){
+        const value = $(this).attr('data-filter')
+        if(value == 'all'){
+            $('.post-box').show('1000')
+        } else {
+            $('.post-box')
+            .not('.' + value)
+            .hide('1000');
+            $('.post-box')
+            .filter('.' + value)
+            .show('1000');
+        }
+    });
+    //Add active to button
+    $('.filter-item').click(function(){
+        $(this).addClass("active-filter").siblings().removeClass("active-filter");
+    })
 });
+let header = document.querySelector('header')
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+})
